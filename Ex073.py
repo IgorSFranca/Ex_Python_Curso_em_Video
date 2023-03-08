d3994ebec54aff8962508089fa8534b48dd0b760
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-07 22:35:47 -0400

    Update Ex073.py
    Correção

--- Ex073.py
+++ Ex073.py
@@ -5,4 +5,4 @@ times = ('Corinthias', 'Flamengo', 'Paysandu', 'Grêmio',
 print(f'Os cinco primeiros colocados são {times[:5]}')
 print(f'Os quatro últimos colocados são {times[-4:]}')
 print(f'A lista dos times em ordem alfabética fica {sorted(times)}')
-# print(f'O time Chapecoense está na {times.index()}º posição.')
+print(f'O time Chapecoense está na {times.index("Chapecoense")}º posição.')
