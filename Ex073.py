d7c369ee81d8941b5e10a25665c38613d4a0e453
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-07 22:30:54 -0400

    Create Ex073.py

---
+++ Ex073.py
@@ -0,0 +1,8 @@
+times = ('Corinthias', 'Flamengo', 'Paysandu', 'Grêmio', 
+         'Internacional', 'Chapecoense', 'Vitória', 'Santos', 
+         'Palmeiras', 'São Paulo', 'Asa de Arapiraca', 'Água Negra', 
+         'Bataguassu', 'Fluminense', 'Botafogo')
+print(f'Os cinco primeiros colocados são {times[:5]}')
+print(f'Os quatro últimos colocados são {times[-4:]}')
+print(f'A lista dos times em ordem alfabética fica {sorted(times)}')
+# print(f'O time Chapecoense está na {times.index()}º posição.')
