28dec14a6df088d6901101aeefaa13a9eefaa58e
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-08 22:15:46 -0400

    Ex80

--- Ex80.py
+++ Ex80.py
@@ -1,14 +1,18 @@
- #lanche.append('item') - adiciona um item no final da lista
-# lanche.insert('posição', 'item') - adiciona um item na posição que você determina 
-# del lanche[3] - apagar um item da lista
-# ou
-# lanche.pop(3) - apagar um item da lista (este indica o índice que você quer eliminar)
-# ou 
-# lanche.remove('item') - apaga um item da lista (este remove o valor que você quer)
-# se usar o lanche.pop(), ele elimna o ultimo elemento
-# Após eliminar um elemento, a lista é ajustada automaticamente 
-# valores = list(range(4, 11)) - cria uma estrutura em lista de um range
-# valores.sort() - ordena os valores dentro da lista
-# valores.sorte(reverse=True) - ordena os valores de forma inversa
-# len(valores) - mosra a quantidade de elementos 
+lista = []
+for valor in range (0, 5):
+    n = int(input('Digite um valor: '))
+    if valor == 0 or valor >= lista[-1]:
+        lista.append(n)
+        print('Adicionado ao final da lista.')
+    else: 
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                print(f'Adicionao a posição {pos} da lista.')
+                break
+            pos += 1
+print('*'*35)
+print(f'Os valores digitados foram {lista}.')
 
+#Não consegui fazer esse caralho. Tive que colar. 
