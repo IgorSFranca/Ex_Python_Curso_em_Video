b2799a001a0e898a412efd39db5a1b7783af635f
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-14 12:21:58 -0400

    Update Ex084.py
    Exercício completo

--- Ex084.py
+++ Ex084.py
@@ -1,19 +1,27 @@
-nome_peso = []
-mais_peso = []
-menos_peso = []
-dados = []
-resp = ''
-tot_pessoas = 0
-while True: 
-    while resp in 'S':
-      dados.append(str(input('Nome: ')))
-      dados.append(float(input('Peso: ')))
-      resp = str(input('Deseja continuar? [S/N]: ')).strip().upper()
-      tot_pessoas += 1
-      nome_peso.append(dados[:])
-      dados.clear()
-      while resp not in 'SN':
-         resp = str(input('Opção não identificada. Deseja Continuar? [S/N]: ')).strip().upper()
+pessoas = []
+temp = []
+pesadas = leves = 0
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    resp = str(input('Deseja continuar? [S/N]: ')).strip().upper()
+    if len(pessoas) == 0:
+        pesadas = leves = temp[1]
+    else:
+        if temp[1] > pesadas:
+            pesadas = temp[1]
+        if temp[1] < leves:
+            leves = temp[1]
+    pessoas.append(temp[:])
+    temp.clear()
+    while resp not in 'SN':
+        resp = str(input('Opção não encontrada. Deseja continuar? [S/N]: ')).strip().upper()
     if resp == 'N':
-      break
-print(f'Foram cadastradas {tot_pessoas} pessoas.')
+        break
+print('-='*30)
+print(f'Foram lançadas {len(pessoas)} pessoas no total')
+for pessoa in pessoas:
+    if pessoa[1] == pesadas:
+        print(f'A pessoa mais pesada é {pessoa[0]} com {pessoa[1]} quilos')
+    elif pessoa[1] == leves:
+        print(f'A pessoa mais leve é a {pessoa[0]} com {pessoa[1]} quilos')
