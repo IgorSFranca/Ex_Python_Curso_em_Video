de5b54eb064f008428d929cee10f8cb057da9364
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-20 16:44:50 -0400

    Incompleto

--- Ex094.py
+++ Ex094.py
@@ -1,23 +1,30 @@
-pessoas = {}
-individuo = []
+individuo = {}
+pessoas = []
+c_pessoas = s_idade = mulheres = 0
 
 while True: 
-    nome = str(input('Nome: '))
-    individuo.append(nome)
-    sexo = str(input('Sexo [M/F]: ')).strip().upper()
-    individuo.append(sexo)
-    while sexo not in 'MF':
+    individuo.clear()
+    individuo ['nome'] = str(input('Nome: '))
+    individuo ['sexo'] = str(input('Sexo [M/F]: ')).strip().upper()
+    while individuo ['sexo'] not in 'MF':
         print('ERRO! Por favor, digite apenas M ou F.')
-        sexo = str(input('Sexo [M/F]: ')).strip().upper()
-        individuo.append(sexo)
-    idade = int(input('Idade: '))
-    individuo.append(idade)
+        individuo ['sexo'] = str(input('Sexo [M/F]: ')).strip().upper()
+    individuo ['idade'] = int(input('Idade: '))
+    s_idade += individuo['idade']
+    pessoas.append(individuo.copy())
     resp = str(input('Quer continuar? [S/N]: ')).strip().upper()
+    c_pessoas += 1
     while resp not in 'SsNn':
         print('ERRO! Responda apenas S ou N.')
         resp = str(input('Quer continuar? [S/N]: ')).strip().upper()
     if resp == 'N':
         break
+media = s_idade/c_pessoas
 
 print('-'*30)
-print(pessoas)
+print(f'A) Ao todo temos {c_pessoas} pessoas cadastradas.')
+print(f'B) A média de idade é {media} anos')
+for pessoa in enumerate(pessoas):
+    if individuo['sexo'] == 'F':
+        mulheres += 1
+print(f'Foram cadastradas {mulheres} mulheres')
