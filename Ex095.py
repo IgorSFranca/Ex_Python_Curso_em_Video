f25c1d18a13e6d34fb706d9c64d3ce74268a262b
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-21 09:33:25 -0400

    Ex095

---
+++ Ex095.py
@@ -0,0 +1,30 @@
+jogador = {}
+ranking = []
+gols = []
+
+while True:
+  jogador.clear()
+  gols.clear()
+  total_gol = 0
+  jogador['nome'] = str(input('Nome do Jogador: '))
+  partida = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+  for c in range (0, partida):
+      gol = int(input(f'--→ Quantos gols na partida {c+1}? '))
+      total_gol += gol
+      gols.append(gol)
+      jogador['gols'] = gols[:]
+      jogador['total'] = total_gol
+  ranking.append(jogador.copy())
+  resp = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+  while resp not in 'SN':
+    print('Opção não encontrada. Digite somente S ou N.')
+    resp = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+  print('-'*30)
+  if resp == 'N':
+     break
+  
+print('-'*30)
+print(f'{"COD ":<4}{"NOME":<15}{"GOLS":<20}{"TOTAL":<5}')
+for i, j in enumerate(ranking):
+   print(f'{i:<4}{j["nome"]:<15}{str(j["gols"]):<20}{j["total"]:<5}')
+print(ranking)
