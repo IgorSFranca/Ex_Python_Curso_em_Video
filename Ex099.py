ab5845eed0c6d7e090126f7fb11bed76396f13d0
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-23 09:20:21 -0400

    Update
    Alterei as tuplas pré-definidas e coloquei valores gerados aleatoriamente

--- Ex099.py
+++ Ex099.py
@@ -15,8 +15,8 @@ def maior(*num):
     print(f'Foram informados {cont} valores ao todo.')
     print(f'O maior valor informado foi {maior}.')    
 
-maior(2, 9, 4, 5, 7, 1)
-maior(4, 7, 0)
-maior(1, 2)
-maior(6)
+maior(randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+maior(randint(1, 10), randint(1, 10), randint(1, 10))
+maior(randint(1, 10), randint(1, 10))
+maior(randint(1, 10))
 maior()
