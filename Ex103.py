36488d7173cf4b2787dbc07531831c64c662b044
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-24 16:44:24 -0400

    Update Ex103.py

--- Ex103.py
+++ Ex103.py
@@ -1,10 +1,10 @@
 def ficha(jogador='', gols=0):
     if jogador == '':
         jogador = '<desconhecido>'
-    # if gols :
-    #     gols = 0
+    if gols.isnumeric() == False:
+        gols = 0
     return f'O jogador {jogador} fez {gols} gol(s) no campeonato.'
 
 jogador = str(input('Nome do Jogador: '))
-gols = int(input('Número de gols: '))
+gols = input('Número de gols: ')
 print(ficha(jogador, gols))
