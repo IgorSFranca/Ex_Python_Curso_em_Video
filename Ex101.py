edd03541af86b5144b9e135881d385ccc0b2673d
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-24 14:26:30 -0400

    Update Ex101.py

--- Ex101.py
+++ Ex101.py
@@ -1,7 +1,18 @@
-def voto():
+from datetime import date
+
+def voto(ano):
     """ Função que recebe como parâmetro o ano de nascimento de uma pessoa, 
     retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO 
     nas eleições.
     """
+    idade = date.today().year - ano
+    print(f'Com {idade} anos: ', end='')
+    if 18 <= idade < 65:
+        print('VOTO OBRIGATÓRIO')
+    elif idade < 18:
+        print('VOTO NEGADO')
+    else:
+        print('VOTO OPCIONAL')
 
-help(voto)
+print('-'*30)
+voto(int(input('Em que ano você nasceu: ')))
