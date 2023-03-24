9d8e080b2776f1ac081cc281e2c4c8318b7997e0
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-24 14:11:54 -0400

    Ex101

---
+++ Ex101.py
@@ -0,0 +1,7 @@
+def voto():
+    """ Função que recebe como parâmetro o ano de nascimento de uma pessoa, 
+    retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO 
+    nas eleições.
+    """
+
+help(voto)
