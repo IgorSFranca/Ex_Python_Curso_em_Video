f6f6c47b344a65f0af338b73c31a4fe9f9c60c63
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-25 08:54:34 -0400

    Update Ex105.py

--- Ex105.py
+++ Ex105.py
@@ -1,9 +1,9 @@
 def notas(*num, sit=False):
     notas = {}
     total_notas = len(num)
-    maior = menor = media = 0
+    maior = menor = soma = media = 0
     notas['total'] = total_notas
-    for valor in range (0, len(num)-1):
+    for valor in range (0, len(num)):
         if valor == 0:
             maior = num[valor]
             menor = num[valor]
@@ -12,7 +12,21 @@ def notas(*num, sit=False):
                 maior = num[valor]
             elif num[valor] < menor:
                 menor = num[valor]
+        soma += num[valor]
+    notas['maior'] = maior
+    notas['menor'] = menor
+    media = soma / total_notas
+    notas['media'] = media
+    if sit == True:
+        if media < 3:
+            notas['situação'] = 'RUIM'
+        elif 3 <= media < 6:
+            notas['situação'] = 'REGULAR'
+        elif 6 <= media < 8:
+            notas['situação'] = 'BOA'
+        else:
+            notas['situação'] = 'EXCELENTE'
     return notas
 
-resp = notas(5.5, 2.5, 1.5, sit=True)
+resp = notas(5.0, 8.5, 10, sit=True)
 print(resp)
