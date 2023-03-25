da2b8aaa8f3a52ad2d770410c329b56b22183e54
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-25 08:43:20 -0400

    Update Ex105.py

--- Ex105.py
+++ Ex105.py
@@ -1,6 +1,18 @@
 def notas(*num, sit=False):
-    print(num)
-
+    notas = {}
+    total_notas = len(num)
+    maior = menor = media = 0
+    notas['total'] = total_notas
+    for valor in range (0, len(num)-1):
+        if valor == 0:
+            maior = num[valor]
+            menor = num[valor]
+        else:
+            if num[valor] > maior:
+                maior = num[valor]
+            elif num[valor] < menor:
+                menor = num[valor]
+    return notas
 
 resp = notas(5.5, 2.5, 1.5, sit=True)
 print(resp)
