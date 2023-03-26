35480bff095ad4dac37b844915f244413c559ba3
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-26 19:28:02 -0400

    Finalizado

--- Ex108/Ex108.py
+++ Ex108/Ex108.py
@@ -1,6 +1,6 @@
 import funcoes_Ex108
 
 preco = float(input('Digite um preço: '))
-print(f'A metade de R$ {preco:.2f} é {funcoes_Ex108.metade(preco)}')
-print(f'O dobro de R$ {preco:.2f} é {funcoes_Ex108.dobro(preco)}')
-print(f'Aumentando 10%, temos {funcoes_Ex108.aumento(preco)}')
+print(f'A metade de {funcoes_Ex108.moeda(preco)} é {funcoes_Ex108.moeda(funcoes_Ex108.metade(preco))}')
+print(f'O dobro de {funcoes_Ex108.moeda(preco)} é {funcoes_Ex108.moeda(funcoes_Ex108.dobro(preco))}')
+print(f'Aumentando 10%, temos {funcoes_Ex108.moeda(funcoes_Ex108.aumento(preco))}')
--- Ex108/funcoes_Ex108.py
+++ Ex108/funcoes_Ex108.py
@@ -1,11 +1,14 @@
-def metade(num):
-    metade = num/2
-    return f'R$ {metade:.2f}'
+def metade(num=0):
+    metade = (num/2)
+    return metade
 
-def dobro(num):
-    dobro = num*2
-    return f'R$ {dobro:.2f}'
+def dobro(num=0):
+    dobro = (num*2)
+    return dobro
 
-def aumento(num):
+def aumento(num=0):
     aumento = num * (num * 10 / 100)
-    return f'R$ {aumento:.2f}'
+    return aumento
+
+def moeda(num=0, moeda='R$'):
+    return f'{moeda} {num:.2f}'.replace('.',',')
