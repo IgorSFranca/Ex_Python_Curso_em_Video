3be4ce97306295778d746c2225f544da17444687
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-03-26 17:07:57 -0400

    Ex107

---
+++ Ex107/Ex107.py
@@ -0,0 +1,6 @@
+import funcoes_Ex107
+
+preco = float(input('Digite um preço: '))
+print(f'A metade de {preco} é {funcoes_Ex107.metade(preco)}')
+print(f'O dobro de {preco} é {funcoes_Ex107.dobro(preco)}')
+print(f'Aumentando 10%, temos {funcoes_Ex107.aumento(preco)}')
---
+++ Ex107/funcoes_Ex107.py
@@ -0,0 +1,11 @@
+def metade(num):
+    metade = num/2
+    return metade
+
+def dobro(num):
+    dobro = num*2
+    return dobro
+
+def aumento(num):
+    aumento = num*1.1
+    return aumento 
